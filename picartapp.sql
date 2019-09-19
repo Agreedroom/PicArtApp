32070cf060b6308dec05e316e19b5b8dc430a532
Agreedroom <agreedroom93102@gmail.com>
2019-09-19 14:38:32 -0500

    Agregue App React

--- picartapp.sql
+++ picartapp.sql
@@ -3,31 +3,49 @@ CREATE TABLE Producto (
   nombre VARCHAR(45) NOT NULL,
   costo INTEGER NOT NULL DEFAULT 0,
   cantidad INTEGER NULL DEFAULT 0,
+  descripcion varchar(255),
   PRIMARY KEY(idProducto)
 );
 
-CREATE TABLE Integrante (
-  idIntegrante INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  nombre VARCHAR(255) NOT NULL,
-  puesto VARCHAR(45) NOT NULL DEFAULT integrante,
-  contraseña VARCHAR(255) NOT NULL DEFAULT admin,
-  PRIMARY KEY(idIntegrante)
+CREATE TABLE Puesto (
+  idPuesto INTEGER(1) UNSIGNED NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(30),
+  descripcion VARCHAR(50),
+  PRIMARY KEY(idPuesto)
 );
 
 CREATE TABLE Cliente (
   idCliente INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  nombre VARCHAR(255) NOT NULL,
+  nombre VARCHAR(100) NOT NULL,
   telefono NUMERIC NOT NULL,
-  correoElectronico VARCHAR(255) NULL,
+  correoElectronico VARCHAR(100) NULL,
+  matricula BIGINT NULL,
+  CURP VARCHAR(18) NULL,
+  Facebook VARCHAR(50) NULL,
+  nombreAval VARCHAR(100) NULL,
+  telefonoAval NUMERIC NULL,
   PRIMARY KEY(idCliente)
+);
+
+CREATE TABLE Integrante (
+  idIntegrante INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  idPuesto INTEGER(1) UNSIGNED NOT NULL,
+  nombre VARCHAR(100) NOT NULL,
+  contraseña VARCHAR(30) NOT NULL DEFAULT 'admin',
+  usuario VARCHAR(14) NOT NULL,
+  PRIMARY KEY(idIntegrante),
+  FOREIGN KEY(idPuesto)
+    REFERENCES Puesto(idPuesto)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
 );
 
 CREATE TABLE Venta (
   idVenta INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   idCliente INTEGER UNSIGNED NOT NULL,
   idIntegrante INTEGER UNSIGNED NOT NULL,
-  fecha DATE NOT NULL,
-  monto total INTEGER UNSIGNED NOT NULL,
+  fecha DATE NOT NULL default CURRENT_TIMESTAMP,
+  monto_total INTEGER UNSIGNED NOT NULL default 0,
   PRIMARY KEY(idVenta),
   INDEX Venta_FKIndex1(idIntegrante),
   INDEX Venta_FKIndex2(idCliente),
@@ -46,6 +64,7 @@ CREATE TABLE VentaDetalle (
   idVenta INTEGER UNSIGNED NOT NULL,
   idProducto INTEGER UNSIGNED NOT NULL,
   cantidad INTEGER UNSIGNED NOT NULL,
+  importe INTEGER UNSIGNED NULL,
   PRIMARY KEY(idVentaDetalle),
   INDEX VentaDetalle_FKIndex1(idVenta),
   INDEX VentaDetalle_FKIndex2(idProducto),
@@ -64,7 +83,7 @@ CREATE TABLE Renta (
   idCliente INTEGER UNSIGNED NOT NULL,
   idIntegrante INTEGER UNSIGNED NOT NULL,
   fecha DATE NOT NULL,
-  fechaDevolución INTEGER UNSIGNED NULL,
+  fechaDevoluciónAcordada DATE NOT NULL,
   montoAcordado INTEGER UNSIGNED NOT NULL,
   PRIMARY KEY(idRenta),
   INDEX Renta_FKIndex1(idIntegrante),
@@ -84,6 +103,7 @@ CREATE TABLE RentaDetalle (
   idRenta INTEGER UNSIGNED NOT NULL,
   idProducto INTEGER UNSIGNED NOT NULL,
   cantidad INTEGER UNSIGNED NULL,
+  importe INTEGER UNSIGNED NULL,
   PRIMARY KEY(idRentaDetalle),
   INDEX RentaDetalle_FKIndex1(idProducto),
   INDEX RentaDetalle_FKIndex2(idRenta),
@@ -97,4 +117,29 @@ CREATE TABLE RentaDetalle (
       ON UPDATE CASCADE
 );
 
+CREATE TABLE Devolucion (
+  idDevolucion INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  Renta_idRenta INTEGER UNSIGNED NOT NULL,
+  fechaDeDevolucion DATE NULL,
+  dias_extra INTEGER(2) UNSIGNED NULL,
+  montoCobrado INTEGER UNSIGNED NULL,
+  PRIMARY KEY(idDevolucion),
+  INDEX Devolucion_FKIndex1(Renta_idRenta),
+  FOREIGN KEY(Renta_idRenta)
+    REFERENCES Renta(idRenta)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
+);
 
+CREATE VIEW VistaVentasDetalle AS
+SELECT vd.cantidad as Cantidad,
+	P.nombre as NombreProducto, 
+	p.costo as ImporteUnitario, 
+	vd.importe as Importe, 
+	v.fecha as Fecha 
+	FROM Producto P, VentaDetalle vd, venta v
+	WHERE (vd.idVenta = v.idVenta) and 
+	(vd.idProducto = p.idProducto);
+
+CREATE VIEW VistaRentasDetalle AS
+	SELECT
