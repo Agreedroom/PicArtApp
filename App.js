f4cf9215c0302f073dbfba171dd530e84b1ac8bc
Agreedroom <agreedroom93102@gmail.com>
2019-09-20 11:51:21 -0500

    Every night i can feel my leg and my arm

--- App.js
+++ App.js
@@ -40,29 +40,29 @@ const App = () => {
           )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionTitle}>I've been thinking too much</Text>
               <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
+  {/**/}<Text style={styles.highlight}>I've been thinking too much</Text> 
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionTitle}>Help me</Text>
               <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
+              {/*  <ReloadInstructions />*/}
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionTitle}>I've been thinking too much</Text>
               <Text style={styles.sectionDescription}>
-                <DebugInstructions />
+               {/* <DebugInstructions />*/}
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionTitle}>I've been thinking too much</Text>
               <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
+                Help me 
               </Text>
+              <Image></Image>
             </View>
             <LearnMoreLinks />
           </View>
