561a2cafd4988961420681df0eebed1305c520f7
xtrs84zk <xtrs84zk@gmail.com>
2019-10-13 19:17:55 -0500

    The eagle has landed

--- webpage/js/global.js
+++ webpage/js/global.js
@@ -68,6 +68,10 @@
     
 
 })(jQuery);
+
+
+
+
 var objUsuarios=[
     {  
         usuario: "Abelmb",
@@ -85,7 +89,7 @@ var objUsuarios=[
     }
 ]
 
-function getInfo(){
+function getInfo() {
     var usuario=document.getElementById("usuario").value
     var password=document.getElementById("password").value
     for(i=0;i<objUsuarios.length;i++){
@@ -98,3 +102,133 @@ function getInfo(){
      alert("Usuario o Contraseña no valida")
     
 }
+
+function validacionUsuario()
+{
+    valido = true;
+
+    if ( document.getElementById("nombre").value == "" )
+    {
+        alert ( "Favor de llenar el espacio del Nombre" );
+        valido = false;
+    }
+    if ( document.getElementById("usuario").value == "" )
+    {
+        alert ( "Favor de llenar el espacio del Usuario" );
+        valido = false;
+    }
+    if ( document.getElementById("password").value == "" )
+    {
+        alert ( "Favor de llenar el espacio de la Contraseña" );
+        valido = false;
+    }
+    if ( document.getElementById("puesto").value == "" )
+    {
+        alert ( "Favor de llenar el espacio del Puesto" );
+        valido = false;
+    }
+    if(valido==true){
+    location.replace("/webpage/regreso_menu_usuarios.html");    
+    }
+}
+
+function validacionMaterial()
+{
+    valido = true;
+
+    if ( document.getElementById("material").value == "" )
+    {
+        alert ( "Favor de llenar el espacio del Material" );
+        valido = false;
+    }
+    if ( document.getElementById("cantidad").value == "" )
+    {
+        alert ( "Espacio de cantidad vacio o invalido" );
+        valido = false;
+    }
+    if ( document.getElementById("precio").value == "" )
+    {
+        alert ( "Espacio de precio vacio o invalido" );
+        valido = false;
+    }
+    
+    if(valido==true){
+    location.replace("/webpage/regreso_menu_material.html");    
+    }
+}
+
+function getLogIn()
+{
+    fetch('http://10.0.105.52:5000/api/integrantes')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(users) {
+    console.log(users);
+        console.log(document.getElementById("usuario").value);
+        if(users._embedded.integrantes.length>0){
+        
+        var user= users._embedded.integrantes.filter(user => user.usuario == document.getElementById("usuario").value )[0]
+        console.log(user);
+        if(user.contrasena== document.getElementById("password").value){
+            console.log("Password bien")
+            location.replace("/webpage/menu.html");
+        }else{
+            console.log("Password mal")
+            alert("Usuario malo o bad password");
+        }
+        }else{
+             alert("pon una password");
+        }
+  });
+    
+}
+
+function createUsuario()
+{
+    fetch('http://10.0.105.52:5000/api/integrantes', {
+  method: 'POST',
+  body:JSON.stringify( {
+    "nombre": document.getElementById("nombre").value,
+    "contrasena": document.getElementById("password").value,
+    "usuario": document.getElementById("usuario").value,
+  }),
+          headers:{
+    'Content-Type': 'application/json'
+  }    
+    })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(users) {
+    console.log(users);
+       location.replace("/webpage/regreso_menu_usuarios.html");   
+  })
+    
+}
+
+
+function createMaterial()
+{
+    fetch('http://10.0.105.52:5000/api/productoes', {
+  method: 'POST',
+  body:JSON.stringify( {
+    "nombre": document.getElementById("material").value,
+    "cantidad": document.getElementById("cantidad").value,
+    "costo_venta": document.getElementById("precio").value,
+      "descripcion": document.getElementById("descripcion").value,
+  }),
+          headers:{
+    'Content-Type': 'application/json'
+  }    
+    })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(users) {
+    console.log(users);
+   location.replace("/webpage/regreso_menu_material.html");      
+  })
+    
+}
+
